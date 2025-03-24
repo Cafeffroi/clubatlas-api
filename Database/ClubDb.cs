aefddb5ee2801751de0deb79141f53104fb5e723
Ringler Francois (LEC-Viveris) <Francois.Ringler.Viveris@liebherr.com>
2025-03-24 15:26:35 +0100

    test minimal API

---
+++ Database/ClubDb.cs
@@ -0,0 +1,13 @@
+﻿using ClubAtlasAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClubAtlasAPI.Database
+{
+    class ClubDb : DbContext
+    {
+        public ClubDb(DbContextOptions<ClubDb> options)
+            : base(options) { }
+
+        public DbSet<Club> Clubs => Set<Club>();
+    }
+}
---
+++ Models/Club.cs
@@ -0,0 +1,21 @@
+﻿namespace ClubAtlasAPI.Models
+{
+    public class Club
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Stub { get; set; }
+        public required string Presentation { get; set; }
+        public DateTime StartDate { get; set; }
+        public required string Adress { get; set; }
+        public required string Phone { get; set; }
+        public required string Email { get; set; }
+        public string? Contact { get; set; }
+        public string? InstagramUrl { get; set; }
+        public string? FacebookUrl { get; set; }
+        public string? TwitterUrl { get; set; }
+        public string? BlueskyUrl { get; set; }
+
+
+    }
+}
